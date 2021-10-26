34b6180bcc237d3397ae765d3f1df26155c2eba1
sprylerIF <52972284+sprylerIF@users.noreply.github.com>
2021-10-26 11:33:25 -0300

    newindexpage

---
+++ newindex.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emanuel R.</title>
+    <link rel="stylesheet" type="text/css" href="css/newcss.css" media="screen" />
+    <script type="text/javascript" src="js/script.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=BenchNine&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="img/ER.ico" type="image/x-icon">
+</head>
+
+<body>
+    <nav>
+    <img style="width: 100px;" src="img/ER.png" alt="Logo">
+    <a class="links" href="#"> Github </a>  
+    </nav>
+
+
+    <div class="centralPage">
+    <p> 
+    Olá, meu nome é <br/>
+    <b style="font-size: 40px;">Emanuel Raimundo</b><br/>
+    e seja bem vindo ao meu portifólio!<br/> 
+    Aqui destaco alguns trabalhos, expêriencias e um pouco da minha história até o dia de hoje.
+    </p>
+    </div>
+
+    <section>   
+       <a href="#"> <img class="seta" src="img/background/arrow.png"> </a>
+</section>
+
+</body>
+
+</html>
