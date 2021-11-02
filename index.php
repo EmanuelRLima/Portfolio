ada71e488427ffe2d274db1e0b8a7ccb0adc743a
sprylerIF <52972284+sprylerIF@users.noreply.github.com>
2021-11-02 11:34:41 -0300

    confirma

--- index.php
+++ index.php
@@ -5,62 +5,120 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Emanuel R.</title>
-    <link rel="stylesheet" type="text/css" href="css/css.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="css/mediacss.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/newcss.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/linkspagecss.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/animation.css" media="screen" />
     <script type="text/javascript" src="js/script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Saira+Extra+Condensed:wght@100;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=BenchNine&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans:wght@300&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="img/ER.ico" type="image/x-icon">
 </head>
 
-<body>
-  <nav class="navbar" id="MNavbar"> 
-    <img class="logo" src="img/ER.png" alt="Logo">
-      <div class="links">
-        <a class="icon" onclick="myFunction()">
-          <i class="fa fa-bars"></i>
-       </a>
-          <a href="index.php">Início</a>
-          <a href="contacts.html">Contatos</a>
-          <a target="_blank" href="https://github.com/sprylerIF">Github</a>
-          <a href="Consideração.html">Obrigado</a>  
-      
-      
-        </div>
-  </nav>
-
-  <div class="bodys">   
-    <p class="Paragrafo"> 
-        Olá, meu nome é <br/>
-      <b style="font-size: 40px;">Emanuel Raimundo</b><br/>
-        e seja bem vindo ao meu portifólio!<br/> 
-        Aqui destaco alguns trabalhos, expêriencias e um pouco da minha história até o dia de hoje.
-      </p>
-    <button class="btn" onclick="openmodal('abrirModal')" > Vamos lá!</button>
-  </div>
-
-  <div >
-    <img class="imgbod" src="img/pictures.png">
-  </div>
-
-  <div id="abrirModal" class="modalcontainer">
-    <div class="modal1">
-      <button title="Fechar"  onclick="closemodal('abrirModal')" class="fechar">x</button>
-        <div> 
-          <div class="curriculo"><img class="mg1"  src="img/trabalho.jpg"> <a href="curriculo.html"><p> <b> Curriculo </b> </p> </a></div>  
-            <div class="pesquisa"> <img class="mg1" src="img/pesquisa.jpg"> <a href="pesquisa.html"> <p> <b> Pesquisa </b> </p> </a></div>
-        </div>
-      <a href="curriculo.html"> <p class="pr"> <b> Curriculo </b> </p> </a>
-      <a href="pesquisa.html"><p class="pr"> <b> Pesquisa </b> </p> </a>
+<body id="trade">
+    <nav>
+        <img class="logo" src="img/ER.png" alt="Logo">
+            <a class="links" href="https://github.com/sprylerIF"> Github </a>  
+    </nav>
+    
+    <div class="centralPage">
+        <p> Olá, meu nome é <br/>
+            <b style="font-size: 40px;">Emanuel Raimundo</b><br/>
+            e seja bem vindo ao meu portifólio!<br/> 
+            Aqui destaco alguns trabalhos, expêriencias e um pouco da minha história até o dia de hoje. </br></br>
+            Vamos lá?
+        </p>
     </div>
-  </div>
 
-  <footer>
-    <img class="mg"  src="img/emanuelra.png">
-  </footer>
+    <section>   
+        <a href="#topicos" onclick="pagedown('projeto');"> <img class="seta" src="img/background/arrow.png"> </a>
+    </section>
 
+    <div id="projeto" class="projetos">
+        <div>
+            <nav>
+                <img class="logo" src="img/ER.png" alt="Logo">
+                    <a class="links" href="https://github.com/sprylerIF"> Github </a>  
+            </nav>
+        </div>
+
+        <div class="topicos1">
+            <h2> Fique a vontade para navegar entre os links abaixo!  </h2>
+                <a class="tplinks" href="#projeto" onclick="pageleft('TC');"> Projetos </a>
+                <a class="tplinks" href="#contatos" onclick="pagedown2('Cont');"> Contatos </a>
+                <a class="tplinks" href="#curriculo" onclick="pageleft2('CR');"> Curriculo </a>
+                <a class="tplinks" href="#Emanuel" onclick="pageup2('Emanuel');"> Quem é Emanuel </a>
+        <section>   
+            <a href="#inicio" onclick="pageup();"> <img class="backseta" src="img/background/arrow.png"> </a>
+        </section>
+    </div>
+</div>
+
+    <div id="TC" class="TCC">
+        <nav>
+            <img class="logo" src="img/ER.png" alt="Logo">
+                <a class="links" href="https://github.com/sprylerIF"> Github </a>  
+        </nav>
+
+        <div class="topicos0">
+            <h2 style="animation-name: op; animation-duration: 2s;"> Olá, você já viu uma oportunidade de se utilizar a tecnologia de radiofrequência ?</h2>
+            <h2 style="animation-name: op; animation-duration: 2s;"> RFID é uma tecnologia que se vem sendo utilizada a pouco tempo, pensando nisso, unimos segurança, controle e monitoramento a esse projeto. </h2>
+            <h2 style="animation-name: op; animation-duration: 2s;"> Esse é o WaySafe</h2>
+            <h2 style="animation-name: op; animation-duration: 2s;"> fique a vontade para assistir o vídeo elaborado para aprensentação a banca avaliativa de TCC's do IFRN e acessar os links do Github ou artigo do projeto. </h2> 
+            
+            <a href="https://docs.google.com/document/d/1bMHZsTcEY0kf6XhewyfNU1frD7-KMIeX9jfpri5uUZc/edit?usp=sharing"> <button class="bu" > Artigo </button> </a>
+            <a href="https://youtu.be/BPQ6NFekGeg"> <button class="bu1" > Youtube </button> </a>
+            <a href="https://github.com/sprylerIF/waysafe"> <button class="bu2" > Código </button> </a>
+        </div>
+        
+        <section>   
+            <a href="#topicos1" onclick="pageleftback();"> <img class="backsetap" src="img/background/arrow.png"> </a>
+        </section>
+    </div>
+
+    <div id="Cont" class="Contacts">
+        <nav>
+            <img class="logo" src="img/ER.png" alt="Logo">
+                <a class="links" href="https://github.com/sprylerIF"> Github </a>  
+        </nav>
+
+        <div class="topicos1">
+            <h2> página de contatos </h2>
+        </div>
+        <section>   
+            <a href="#topicos1" onclick="pagedownback();"> <img class="backsetac" src="img/background/arrow.png"> </a>
+        </section>
+    </div>
+
+    <div id="CR" class="Curriculo">
+        <nav>
+            <img class="logo" src="img/ER.png" alt="Logo">
+                <a class="links" href="https://github.com/sprylerIF"> Github </a>  
+        </nav>
+
+        <div class="topicos1">
+            <h2> curriculo! </h2>
+        </div>
+        <section>   
+            <a href="#topicos1" onclick="pageleftback2();"> <img class="backsetap" src="img/background/arrow.png"> </a>
+        </section>
+    </div>
+
+    <div id="Emanuel" class="Quem">
+        <nav>
+            <img class="logo" src="img/ER.png" alt="Logo">
+                <a class="links" href="https://github.com/sprylerIF"> Github </a>  
+        </nav>
+
+        <div class="topicos1">
+            <h2> Página sobre! </h2>
+        </div>
+        <section>   
+            <a href="#topicos1" onclick="pagedownback2();"> <img class="backsetac" src="img/background/arrow.png"> </a>
+        </section>
+    </div>
 </body>
 
 </html>
--- js/script.js
+++ js/script.js
@@ -1,22 +1,121 @@
-function openmodal(mn){
-  let modal1 = document.getElementById(mn);
-   if (typeof modal1 == 'undefined' || modal1 === null)
+function pagedown(mn){
+  let topicos0 = document.getElementById(mn);
+    if (typeof topicos0 == 'undefined' || topicos0 === null)
     return;
-      modal1.style.display = 'flex';
-}
+      topicos0.style.display = 'flex';
 
-function closemodal(mn){
-  let modal1 = document.getElementById(mn);
-    if (typeof modal1 == 'undefined' || modal1 === null)
-      return;
-        modal1.style.display = 'none';
-}
-
-function myFunction() {
-  var x = document.getElementById("MNavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
+  var x = document.getElementById("projeto");
+    if (x.className === "projetos") {
+        x.className += "";
     }else {
-      x.className = "navbar";
+      x.className = "projetos";
+  }
+}
+
+function pageup() {
+  if (typeof trade == 'undefined' || trade === null)
+    return;
+      trade.style.background = '#0d0d0d';
+  var x = document.getElementById("projeto");
+    if (x.className === "projetos") {
+      x.className += " projetosup";
+    }else {
+      x.className = "projetos";
+  }
+}
+
+function pageleft(mn){
+  let topicos = document.getElementById(mn);
+  if (typeof trade == 'undefined' || trade === null)
+    if (typeof topicos == 'undefined' || topicos === null)
+    return;
+      topicos.style.display = 'flex';
+      trade.style.background = '#BB405E';
+  var x = document.getElementById("TC");
+    if (x.className === "TCC") {
+        x.className += "";
+    }else {
+      x.className = "TCC";
+  }
+}
+
+function pageleftback() {
+  var x = document.getElementById("TC");
+    if (x.className === "TCC") {
+      x.className += " projetosleft";
+    }else {
+      x.className = "TCC";
+  }
+}
+
+function pagedown2(mn){
+  let topicos = document.getElementById(mn);
+  if (typeof trade == 'undefined' || trade === null)
+    if (typeof topicos == 'undefined' || topicos === null)
+    return;
+      topicos.style.display = 'flex';
+      trade.style.background = '#D2691E';
+  var x = document.getElementById("Cont");
+    if (x.className === "Contacts") {
+        x.className += "";
+    }else {
+      x.className = "Contacts";
+  }
+}
+
+function pagedownback() {
+  var x = document.getElementById("Cont");
+    if (x.className === "Contacts") {
+      x.className += " Contactsright";
+    }else {
+      x.className = "Contacts";
+  }
+}
+
+function pageleft2(mn){
+  let topicos = document.getElementById(mn);
+    if (typeof topicos == 'undefined' || topicos === null)
+    if (typeof trade == 'undefined' || trade === null)
+    return;
+      topicos.style.display = 'flex';
+      trade.style.background = '#B0C4DE';
+  var x = document.getElementById("CR");
+    if (x.className === "Curriculo") {
+        x.className += "";
+    }else {
+      x.className = "Curriculo";
+  }
+}
+
+function pageleftback2() {
+  var x = document.getElementById("CR");
+    if (x.className === "Curriculo") {
+      x.className += " projetosleft2";
+    }else {
+      x.className = "Curriculo";
+  }
+}
+
+function pageup2(mn){
+  let topicos = document.getElementById(mn);
+    if (typeof topicos == 'undefined' || topicos === null)
+    if (typeof trade == 'undefined' || trade === null)
+    return;
+      topicos.style.display = 'flex';
+      trade.style.background = '#191970';
+  var x = document.getElementById("Emanuel");
+    if (x.className === "Quem") {
+        x.className += "";
+    }else {
+      x.className = "Quem";
+  }
+}
+
+function pagedownback2() {
+  var x = document.getElementById("Emanuel");
+    if (x.className === "Quem") {
+      x.className += " projetosleft3";
+    }else {
+      x.className = "Contacts";
   }
 }
